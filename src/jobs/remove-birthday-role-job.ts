2f835f3c076c1e28ec46ae01f10c5b1b9946a688
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-05-30 21:07:20 -0400

    Completed rough draft of remove birthday role job

--- src/jobs/remove-birthday-role-job.ts
+++ src/jobs/remove-birthday-role-job.ts
@@ -1,8 +1,10 @@
 import { Client } from 'discord.js';
+import moment from 'moment';
 import schedule from 'node-schedule';
 import { createRequire } from 'node:module';
 
 import { GuildData } from '../models/database/guild-models.js';
+import { UserData } from '../models/database/user-models.js';
 import { GuildRepo } from '../services/database/repos/guild-repo.js';
 import { UserRepo } from '../services/database/repos/user-repo.js';
 import { Logger } from '../services/index.js';
@@ -24,10 +26,31 @@ export class RemoveBirthdayRoleJob implements Job {
     public async run(): Promise<void> {
         // Collection of guilds
         let guildCache = this.client.guilds.cache;
-        let _discordIds = guildCache.map(guild => guild.id);
+        let guildDiscordIds = guildCache.map(guild => guild.id);
 
         // TODO: Get all the guilds from the database
-        let guildDatas: GuildData[] = [];
+        let guildDatas: GuildData[] = await this.guildRepo.getAllGuilds(guildDiscordIds);
+
+        let now = moment();
+        let today = now.clone().format('MM-DD');
+        let tomorrow = now.clone().add(1, 'day').format('MM-DD');
+        let yesterday = now.clone().subtract(1, 'day').format('MM-DD');
+
+        // Instead of getting the users for each guild, we can just get the users with a birthday,
+        // so we know who not to take the birthday role from
+        let birthdayUserData: UserData[] = [
+            ...(await this.userRepo.getUsersWithBirthday(today)),
+            ...(await this.userRepo.getUsersWithBirthday(tomorrow)),
+            ...(await this.userRepo.getUsersWithBirthday(yesterday)),
+        ];
+
+        if (
+            !TimeUtils.isLeap(now.year()) &&
+            (today === '02-28' || tomorrow === '02-28' || yesterday === '02-28')
+        ) {
+            // Add leap year birthdays to list
+            birthdayUserData.push(...(await this.userRepo.getUsersWithBirthday('02-29')));
+        }
 
         for (let guildData of guildDatas) {
             try {
@@ -35,10 +58,7 @@ export class RemoveBirthdayRoleJob implements Job {
                 // loop through the guilds roles
                 let guild = await this.client.guilds.fetch(guildData.GuildDiscordId);
 
-                let members = guild.members.cache;
-
-                // TODO: could do this in one call before the guildData loop and filter down to the users in this guild
-                let userData = await this.userRepo.getAllUsers(members.map(member => member.id));
+                let members = guild.members.cache.map(m => m);
 
                 let role = guild.roles.cache.get(guildData.BirthdayRoleDiscordId);
 
@@ -50,27 +70,40 @@ export class RemoveBirthdayRoleJob implements Job {
                 // filter out the members who don't have the birthday role
                 let membersWithRole = members.filter(member => member.roles.cache.has(role.id));
 
-                // Get the celebration data for these members
-                let membersWithBirthdayTodayOrYesterday = membersWithRole.filter(member =>
+                let membersWithRoleIds = membersWithRole.map(member => member.id);
+
+                // Remove anyone not in membersWithRolesIds (Members not in this server and who don't have the birthday role)
+                let memberUserDatas = birthdayUserData.filter(userData =>
+                    membersWithRoleIds.includes(userData.UserDiscordId)
+                );
+
+                let membersWithBirthdayTodayOrYesterday = members.filter(member =>
                     CelebrationUtils.isBirthdayTodayOrYesterday(
-                        userData.find(data => data.UserDiscordId === member.id),
+                        memberUserDatas.find(data => data.UserDiscordId === member.id),
                         guildData
                     )
                 );
 
                 let birthdayMemberStatuses = membersWithBirthdayTodayOrYesterday.map(m =>
                     CelebrationUtils.getBirthdayMemberStatus(
-                        userData.find(data => data.UserDiscordId === m.id),
+                        memberUserDatas.find(data => data.UserDiscordId === m.id),
                         m,
                         guildData
                     )
                 );
 
+                // Get a list of people who need the birthday role removed
+                let membersToRemoveRole = membersWithRole.filter(
+                    member =>
+                        birthdayMemberStatuses.find(status => status.member.id === member.id)
+                            .needsRoleRemoved
+                );
+
                 // Remove the birthday role from the members
-                for (let birthdayMemberStatus of birthdayMemberStatuses) {
+                for (let member of membersToRemoveRole) {
                     if (counter >= calculatedMax) break;
-                    if (birthdayMemberStatus.needsRoleRemoved) {
-                        ActionUtils.removeRole(birthdayMemberStatus.member, role);
+                    if (member.roles.cache.has(role.id)) {
+                        await ActionUtils.removeRole(member, role);
                         counter++;
                     }
                 }
